e908d7008d1378a63defb72f19cb6c028db3715c
clucidojr123 <clucidojr123@gmail.com>
2022-02-24 21:00:46 -0500

    implement image model

--- src/models/image.ts
+++ src/models/image.ts
@@ -1,10 +1,31 @@
 import { Document, Types, Schema, model } from "mongoose";
 
 export interface IImage extends Document {
+    name: string;
+    imageURL: string;
+    tags: string[];
+    uploadedBy: Types.ObjectId;
     createdAt: Date;
 }
 
 const imageSchema = new Schema<IImage>({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        default: [],
+    },
+    uploadedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
