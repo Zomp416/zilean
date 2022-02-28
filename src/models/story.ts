c1c8221284268933adfa0e0e33e096d2650b23cc
Matt Ho <mattho@Matts-MacBook-Pro.local>
2022-02-27 23:11:27 -0500

    First Commit

--- src/models/story.ts
+++ src/models/story.ts
@@ -1,13 +1,79 @@
 import { Document, Types, Schema, model } from "mongoose";
 
 export interface IStory extends Document {
+    uuid: string;
     createdAt: Date;
+    title: string, 
+    description: string,
+    author: Types.ObjectId;
+    views: number;
+    ratingTotal: number;
+    ratingCount: number;
+    comments: {
+        text: string;
+        author: Types.ObjectId;
+    }[];
+    publishedAt?: Date;
+    story: string;
 }
 
 const storySchema = new Schema<IStory>({
+    uuid:{
+        type: String,
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    ratingTotal:{ 
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    ratingCount: {
+        type: Number,
+        default: 0,
+        required: true,
+    },
+    coments: {
+        type: [
+            {
+                text: {
+                    type: String,
+                    required: true,
+                },
+                author: {
+                    type: Schema.Types.ObjectId,
+                    ref: "User",
+                },
+            },
+        ],
+        required: true,
+    },
+    publishedAt:{
+        type: Date,
+    },
+    story: {
+        type: String,
+        required: true,
     },
 });
 
