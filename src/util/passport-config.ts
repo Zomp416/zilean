e26ae78b611a86a39b0fbdee75f4a16b4b7cce71
clucidojr123 <clucidojr123@gmail.com>
2022-03-03 19:31:08 -0500

    start adding passport stuff

---
+++ src/util/passport-config.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import { NativeError } from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import passport from "passport";
+import passportLocal from "passport-local";
+import User, { IUser } from "../models/user";
+
+const LocalStrategy = passportLocal.Strategy;
+
+passport.serializeUser<any, any>((_, user, done) => {
+    done(undefined, user);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err: NativeError, user: IUser) => done(err, user));
+});
+
+// Local Strategy
+passport.use(
+    new LocalStrategy(
+        { usernameField: "username" },
+        async (username, password, done) => {
+            // Find user with given username
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(undefined, false, { message: "User not found." });
+            }
+            const result = await bcrypt.compare(password, user.password);
+            if (result) {
+                return done(undefined, user);
+            } else {
+                return done(undefined, false, {
+                    message: "Invalid username or password.",
+                });
+            }
+        }
+    )
+);
+
+export const isAuthenticated = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // TODO figure out best way to handle
+    res.redirect("/login");
+};
+
+module.exports = passport;
