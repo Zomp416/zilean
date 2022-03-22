edf35f171b101d978e54f6ddd120cd10959e2e18
keith <keithohno@gmail.com>
2022-03-22 16:22:36 -0700

    renamed variable

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import createApp from "./app";
 
 async function main() {
     const mongo_uri = process.env.MONGO_URI;
-    const secret = process.env.SESSION_SECRET;
+    const session_secret = process.env.SESSION_SECRET;
 
     if (!mongo_uri) throw new Error("No MongoDB URI");
-    if (!secret) throw new Error("No secret");
+    if (!session_secret) throw new Error("No secret");
 
     await mongoose.connect(mongo_uri);
 
-    const app = createApp(mongo_uri, secret);
+    const app = createApp(mongo_uri, session_secret);
 
     app.listen(process.env.PORT, () => {
         console.log(`🚀 Zilean backend service now listening on port ${process.env.PORT}`);
