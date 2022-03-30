9034e7d0c12fafd65915cc22f08255ebff328150
Matt Ho <mattho@Matts-MacBook-Pro.local>
2022-03-29 22:39:22 -0400

    Made it work with story

--- src/routes/story.ts
+++ src/routes/story.ts
@@ -1,89 +1,89 @@
 import express from "express";
 import { isAuthenticated } from "../util/passport-config";
-import Comic, { IComic } from "../models/comic";
+import Story, { IStory } from "../models/story";
 import User, { IUser } from "../models/user";
 
 const router = express.Router();
 
-// GET COMIC
+// GET STORY
 router.get("/:id", async (req, res, next) => {
-    const comic = await Comic.findById(req.params.id);
-    if (!comic) {
-        res.status(400).json({ error: "No comic found" });
+    const story = await Story.findById(req.params.id);
+    if (!story) {
+        res.status(400).json({ error: "No story found" });
         return next();
     }
-    res.status(200).json(comic);
+    res.status(200).json(story);
     return next();
 });
 
-// CREATE COMIC - AUTH
+// CREATE STORY - AUTH
 router.post("/", isAuthenticated, async (req, res, next) => {
     const user = req.user as IUser;
     if (!user.verified) {
-        res.status(401).json({ error: "Must be verified to create a comic." });
+        res.status(401).json({ error: "Must be verified to create a story." });
         return next();
     }
 
-    const newComic = new Comic({
-        title: "Unnamed Comic",
+    const newStory = new Story({
+        title: "Unnamed Story",
         author: user._id,
     });
 
-    await newComic.save();
-    res.status(200).json(newComic);
+    await newStory.save();
+    res.status(200).json(newStory);
     return next();
 });
 
-// UPDATE COMIC - AUTH
+// UPDATE STORY - AUTH
 router.put("/:id", isAuthenticated, async (req, res, next) => {
     const user = req.user as IUser;
-    const comic = await Comic.findById(req.params.id);
+    const story = await Story.findById(req.params.id);
 
-    if (!comic) {
-        res.status(400).json({ error: "No comic found" });
+    if (!story) {
+        res.status(400).json({ error: "No story found" });
         return next();
     }
 
     if (!user.verified) {
-        res.status(401).json({ error: "Must be verified to update a comic." });
+        res.status(401).json({ error: "Must be verified to update a story." });
         return next();
     }
 
-    if (comic.author !== user._id) {
-        res.status(401).json({ error: "Must be the author to update comic." });
+    if (story.author !== user._id) {
+        res.status(401).json({ error: "Must be the author to update story." });
         return next();
     }
 
-    // TODO assert that req.body.comic is actually a comic
-    const updatedComic = req.body.comic as IComic;
-    await comic.update(updatedComic);
+    // TODO assert that req.body.story is actually a story
+    const updatedStory = req.body.story as IStory;
+    await story.update(updatedStory);
 
-    res.status(200).json(comic);
+    res.status(200).json(story);
     return next();
 });
 
-// DELETE COMIC - AUTH
+// DELETE STORY - AUTH
 router.delete("/:id", isAuthenticated, async (req, res, next) => {
     const user = req.user as IUser;
-    const comic = await Comic.findById(req.params.id);
+    const story = await Story.findById(req.params.id);
 
-    if (!comic) {
-        res.status(400).json({ error: "No comic found" });
+    if (!story) {
+        res.status(400).json({ error: "No story found" });
         return next();
     }
 
     if (!user.verified) {
-        res.status(401).json({ error: "Must be verified to delete a comic." });
+        res.status(401).json({ error: "Must be verified to delete a story." });
         return next();
     }
 
-    if (comic.author !== user._id) {
-        res.status(401).json({ error: "Must be the author to delete comic." });
+    if (story.author !== user._id) {
+        res.status(401).json({ error: "Must be the author to delete story." });
         return next();
     }
 
-    await comic.delete();
-    res.status(200).json({ msg: "Successfully deleted comic." });
+    await story.delete();
+    res.status(200).json({ msg: "Successfully deleted story." });
     return next();
 });
 
